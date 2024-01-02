2b1c54bd088627ac4eb64405cceb6ba8505e7ec3
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-03 00:29:05 +0530

    add cursor component

---
+++ src/utils/asset_util.js
@@ -0,0 +1,10 @@
+export const assets = {
+    chrome:
+      "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Ff4c72f33c40b4b7bb220cffbc17ef2c2",
+    mobile:
+      "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fc9bace408c6a4ab6b13a24b947f96517",
+    desktop:
+      "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2F0cd3a13636be4aff87208714780c356c",
+    cursor:
+      "https://cdn.builder.io/api/v1/image/assets%2FYJIGb4i01jvw0SRdL5Bt%2Fbba6d6b3473d436098e3637650f64ee2",
+  };
