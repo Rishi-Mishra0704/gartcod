3efb023c1214d704d93a7d144842fa830a0cc3ee
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-02 21:14:03 +0530

    remove everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import IconComponent from '@/components/IconComponent';
+
 
 export default function Home() {
-  const [icons, setIcons] = useState<('chrome' | 'terminal' | 'mobile')[]>([
-    'chrome',
-    'terminal',
-    'mobile',
-  ]);
-  const [currentColor, setCurrentColor] = useState<string>('#ffeb3b'); // Initial color
-
+ 
   return (
-   <></>
-  )
+    <>
+
+    </>
+  );
 }
